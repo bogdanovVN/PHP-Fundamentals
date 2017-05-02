aab0b3669eb1a714b9bb40b8cf5d5bbba63b91be
bogdanovVN <hrisvn@gmail.com>
2017-05-02 12:09:20 +0300

    Some improvements

--- 03.Arrays and Multidimensional arrays/Exercise/02. RotateAndSum.php
+++ 03.Arrays and Multidimensional arrays/Exercise/02. RotateAndSum.php
@@ -1,27 +1,25 @@
 <?php
-$inputArray = explode(" ", fgets(STDIN));
-$timesToRotated = intval(fgets(STDIN));
+$inputArray = array_map('trim', explode(" ", fgets(STDIN)));
+$timesToRotate = intval(trim(fgets(STDIN)));
 $countOfElements = count($inputArray);
 $lastElement = "";
 $finalArray = [];
 $arrayOfSum = [];
-for ($i = 0; $i < $timesToRotated; $i++){
-    //$finalArray[$i] = [];  // Making multidimentional array
+for ($i = 0; $i < $timesToRotate; $i++) {
+    //$finalArray[$i] = [];  // Making multidimensional array
     $lastElement = $inputArray[$countOfElements - 1];
-    array_splice($inputArray, $countOfElements - 1, 1);    // array_splice($array, $startIndex, $length) – removes the elements in the given range
-    array_splice($inputArray, 0, 0, $lastElement);         // array_splice($array, $startIndex, $length, $element)
-    $finalArray[$i] = $inputArray;                         // Making Multidimentional array from arrays.
+    array_splice($inputArray, - 1); // array_splice($array, $startIndex, $length)– removes the elements in the given range
+    array_splice($inputArray, 0, 0, $lastElement); // array_splice($array, $startIndex, $length, $element)
+    $finalArray[$i] = $inputArray;  // Making Multidimensional array from arrays.
 }
-for ($i = 0; $i <= $countOfElements - 1; $i++){
-    // Sum all elemenets from the arrays in the Multidimentional array & making new sum array.
+for ($i = 0; $i < $countOfElements; $i++) {
+    // Sum all elements from the arrays in the Multidimensional array & making new sum array
     $sum = 0;
-    for ($j = 0; $j < $timesToRotated; $j++){   // Sum all numbers in each column[$j].
+    for ($j = 0; $j < $timesToRotate; $j++) {  // Sum all numbers in each column[$j].
         $sum += intval($finalArray[$j][$i]);
         $arrayOfSum[$i] = $sum;
     }
 }
-$outputString = "";
 foreach ($arrayOfSum as $item) {
-    $outputString .= "{$item} ";
+    echo "{$item} ";
 }
-echo $outputString;
